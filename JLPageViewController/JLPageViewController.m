de2adcea52ac6c7824dd7fc05c4a005fff7b89dc
Jangsy7883 <studio@jangseog-yong-ui-MacBook-Pro.local>
2017-03-05 14:06:22 +0900

    소스정리

--- JLPageViewController/JLPageViewController.m
+++ JLPageViewController/JLPageViewController.m
@@ -19,21 +19,16 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 @implementation UIViewController (JLPageViewController)
 
-- (NSUInteger)jl_pageIndex
-{
-    id obj = objc_getAssociatedObject(self, PageIndexPropertyKey);
+- (NSUInteger)jl_pageIndex {    id obj = objc_getAssociatedObject(self, PageIndexPropertyKey);
     
-    if ([obj isKindOfClass:[NSNumber class]])
-    {
+    if ([obj isKindOfClass:[NSNumber class]]) {
         return [obj unsignedIntegerValue];
     }
     return NSNotFound;
 }
 
-- (void)setJl_pageIndex:(NSUInteger)pageIndex
-{
+- (void)setJl_pageIndex:(NSUInteger)pageIndex {
     NSNumber *num = @(pageIndex);
-    
     objc_setAssociatedObject(self, PageIndexPropertyKey, num, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
@@ -46,29 +41,25 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 @implementation JLCustomPageViewController
 
-- (void)setViewControllers:(NSArray*)viewControllers direction:(UIPageViewControllerNavigationDirection)direction animated:(BOOL)animated completion:(void (^)(BOOL))completion
-{
-    if (animated)
-    {
-        [super setViewControllers:viewControllers direction:direction animated:YES completion:^(BOOL finished)
-        {
-            if (finished)
-            {
+- (void)setViewControllers:(NSArray*)viewControllers
+                 direction:(UIPageViewControllerNavigationDirection)direction
+                  animated:(BOOL)animated
+                completion:(void (^)(BOOL))completion {
+    if (animated) {
+        [super setViewControllers:viewControllers direction:direction animated:YES completion:^(BOOL finished) {
+            if (finished) {
                 dispatch_async(dispatch_get_main_queue(), ^{
                     [super setViewControllers:viewControllers direction:direction animated:NO completion:completion];
                 });
             }
-            else
-            {
-                if (completion != NULL)
-                {
+            else {
+                if (completion != NULL) {
                     completion(finished);
                 }
             }
         }];
     }
-    else
-    {
+    else {
         [super setViewControllers:viewControllers direction:direction animated:NO completion:completion];
     }
 }
@@ -88,45 +79,37 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - dealloc
 
-- (void)dealloc
-{
+- (void)dealloc {
     [self removeObservers];
 }
 
 #pragma mark - init
 
-- (void)baseInit
-{
+- (void)baseInit {
     _transitionInProgress = NO;
     _currentIndex = NSNotFound;
     _nextIndex = NSNotFound;
 }
 
-- (instancetype)init
-{
+- (instancetype)init {
     self = [super init];
-    if (self)
-    {
+    if (self) {
         [self baseInit];
     }
     return self;
 }
 
-- (instancetype)initWithCoder:(NSCoder *)aDecoder
-{
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
-    if (self)
-    {
+    if (self) {
         [self baseInit];
     }
     return self;
 }
 
-- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self)
-    {
+    if (self) {
         [self baseInit];
     }
     return self;
@@ -134,8 +117,7 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - view lifecycle
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     
     //PAGE VIEW CONTROLLER
@@ -156,27 +138,21 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
     [self reloadData];
 }
 
-- (void)viewWillLayoutSubviews
-{
+- (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
-    
     self.pageViewController.view.frame = self.view.bounds;
 }
 
 #pragma mark - RELOAD
 
-- (void)reloadData
-{
+- (void)reloadData {
     NSUInteger index = _currentIndex;
     
-    if (_currentIndex == NSNotFound)
-    {
-        if ([self.dataSource respondsToSelector:@selector(defaultPageIndexForPageViewController:)])
-        {
+    if (_currentIndex == NSNotFound) {
+        if ([self.dataSource respondsToSelector:@selector(defaultPageIndexForPageViewController:)]) {
             index = [self.dataSource defaultPageIndexForPageViewController:self];
         }
-        else
-        {
+        else {
             index = 0;
         }
     }
@@ -187,8 +163,7 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - observer
 
-- (void)addObservers
-{
+- (void)addObservers {
     @try
     {
         [self.scrollView addObserver:self
@@ -199,8 +174,7 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
     @catch (NSException *exception) {};
 }
 
-- (void)removeObservers
-{
+- (void)removeObservers {
     @try
     {
         [self.scrollView removeObserver:self
@@ -210,16 +184,14 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
     @catch (NSException *exception) {};
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     if ([object isKindOfClass:[UIScrollView class]]
         && [keyPath isEqualToString:NSStringFromSelector(@selector(contentOffset))])
     {
         CGPoint new = [change[NSKeyValueChangeNewKey] CGPointValue];
         CGPoint old = [change[NSKeyValueChangeOldKey] CGPointValue];
         
-        if (CGPointEqualToPoint(new, old) == NO)
-        {
+        if (CGPointEqualToPoint(new, old) == NO) {
             [self scrollViewDidScroll:object];
         }
     }
@@ -227,26 +199,22 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - PageViewController DataSource
 
-- (UIViewController*)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
-{
+- (UIViewController*)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
     NSInteger index = (viewController.jl_pageIndex == NSNotFound) ? 0 : viewController.jl_pageIndex+1;
     UIViewController *afterViewController = nil;
     
-    if (index >= 0)
-    {
+    if (index >= 0) {
         afterViewController = [self viewControllerForIndex:index];
     }
     
     return afterViewController;
 }
 
-- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
-{
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
     NSInteger index = (viewController.jl_pageIndex == NSNotFound) ? 0 : viewController.jl_pageIndex-1;
     UIViewController *beforeViewController = nil;
     
-    if (index >= 0)
-    {
+    if (index >= 0) {
         beforeViewController = [self viewControllerForIndex:index];
     }
     
@@ -255,23 +223,18 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma  mark - PageViewController Delegate
 
-- (void)pageViewController:(UIPageViewController *)pageViewController willTransitionToViewControllers:(NSArray *)pendingViewControllers
-{
+- (void)pageViewController:(UIPageViewController *)pageViewController willTransitionToViewControllers:(NSArray *)pendingViewControllers {
     _transitionInProgress = YES;
     
-    
     UIViewController *viewController = pendingViewControllers.firstObject;
-    
     _nextIndex = viewController.jl_pageIndex;
 }
 
 - (void)pageViewController:(UIPageViewController *)pageViewController
         didFinishAnimating:(BOOL)finished
    previousViewControllers:(NSArray<UIViewController *> *)previousViewControllers
-       transitionCompleted:(BOOL)completed
-{
-    if (completed)
-    {
+       transitionCompleted:(BOOL)completed {
+    if (completed) {
         [self didFinishTransition];
     }
     
@@ -279,25 +242,21 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
     _transitionInProgress = NO;
 }
 
-- (void)didFinishTransition
-{
+- (void)didFinishTransition {
     NSUInteger fromIndex = _currentIndex;
 
     _currentIndex = [self indexPathForPageContainingViewController:self.pageViewController.viewControllers.firstObject];
     
-    for (UIViewController *viewController in self.pageViewController.childViewControllers)
-    {
+    for (UIViewController *viewController in self.pageViewController.childViewControllers) {
         UIScrollView *scrollView = [self scrollViewForContainingViewController:viewController];
         
-        if (scrollView)
-        {
+        if (scrollView) {
             scrollView.scrollsToTop = (_currentIndex == viewController.jl_pageIndex);
         }
     }
     
     if (_currentIndex != NSNotFound
-        && [self.delegate respondsToSelector:@selector(pageViewController:didChangeToCurrentIndex:fromIndex:)])
-    {
+        && [self.delegate respondsToSelector:@selector(pageViewController:didChangeToCurrentIndex:fromIndex:)]) {
         [self.delegate pageViewController:self didChangeToCurrentIndex:self.currentIndex fromIndex:fromIndex];
     }
 
@@ -306,25 +265,20 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - ScrollView
 
-- (UIScrollView*)scrollViewForContainingViewController:(UIViewController*)viewController
-{
-    if ([viewController.view isKindOfClass:[UIScrollView class]])
-    {
+- (UIScrollView*)scrollViewForContainingViewController:(UIViewController*)viewController {
+    if ([viewController.view isKindOfClass:[UIScrollView class]]) {
         return (UIScrollView*)viewController.view;
     }
     
-    for (UIScrollView* scrollView in viewController.view.subviews)
-    {
-        if ([scrollView isKindOfClass:[UIScrollView class]])
-        {
+    for (UIScrollView* scrollView in viewController.view.subviews) {
+        if ([scrollView isKindOfClass:[UIScrollView class]]) {
             return scrollView;
         }
     }
     return nil;
 }
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     if ([self.delegate respondsToSelector:@selector(pageViewController:didScrollToCurrentPosition:)] )
     {
         NSUInteger nextIndex = _nextIndex;
@@ -357,17 +311,13 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - VIEWCONTROLLER
 
-- (NSUInteger)indexPathForPageContainingViewController:(UIViewController*)viewController
-{
+- (NSUInteger)indexPathForPageContainingViewController:(UIViewController*)viewController {
     return viewController.jl_pageIndex;
 }
 
-- (UIViewController*)viewControllerForIndex:(NSInteger)index
-{
+- (UIViewController*)viewControllerForIndex:(NSInteger)index {
     UIViewController *viewController = nil;
-    
-    if ([self.dataSource respondsToSelector:@selector(pageViewController:viewControllerForIndex:)])
-    {
+    if ([self.dataSource respondsToSelector:@selector(pageViewController:viewControllerForIndex:)]) {
         viewController = [self.dataSource pageViewController:self viewControllerForIndex:index];
     }
     
@@ -378,25 +328,19 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - SETTERS
 
-- (void)setCurrentIndex:(NSUInteger)currentIndex
-{
+- (void)setCurrentIndex:(NSUInteger)currentIndex {
     [self setCurrentIndex:currentIndex animated:NO];
 }
 
-- (void)setCurrentIndex:(NSUInteger)currentIndex animated:(BOOL)animated
-{
-    if (_currentIndex != currentIndex && _transitionInProgress == NO)
-    {
-        if (self.pageViewController == nil)
-        {
+- (void)setCurrentIndex:(NSUInteger)currentIndex animated:(BOOL)animated {
+    if (_currentIndex != currentIndex && _transitionInProgress == NO) {
+        if (self.pageViewController == nil) {
             _currentIndex = currentIndex;
         }
-        else
-        {
+        else {
             UIViewController *viewController = [self viewControllerForIndex:currentIndex];
             
-            if (viewController)// && [self.pageViewController.viewControllers.firstObject isEqual:viewController] == NO)
-            {
+            if (viewController) {// && [self.pageViewController.viewControllers.firstObject isEqual:viewController] == NO)
                 _nextIndex = currentIndex;
                 
                 __weak JLPageViewController *blocksafeSelf = self;
@@ -406,9 +350,7 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
                                                   direction:direction
                                                    animated:animated
                                                  completion:^(BOOL finished) {
-                                                     
-                                                     if (finished)
-                                                     {
+                                                     if (finished) {
                                                          [blocksafeSelf didFinishTransition];
                                                      }
                                                  }];
@@ -438,29 +380,22 @@ static void * PageIndexPropertyKey = &PageIndexPropertyKey;
 
 #pragma mark - GETTERS
 
-- (NSArray<UIViewController*>*)viewControllers
-{
+- (NSArray<UIViewController*>*)viewControllers {
     return self.pageViewController.childViewControllers;
 }
 
-- (UIViewController*)currentViewController
-{
-    for (UIViewController*viewController in self.pageViewController.childViewControllers)
-    {
-        if (viewController.jl_pageIndex == _currentIndex)
-        {
+- (UIViewController*)currentViewController {
+    for (UIViewController*viewController in self.pageViewController.childViewControllers) {
+        if (viewController.jl_pageIndex == _currentIndex) {
             return viewController;
         }
     }
     return nil;
 }
 
-- (UIScrollView *)scrollView
-{
-    for (UIView *subview in self.pageViewController.view.subviews)
-    {
-        if ([subview isKindOfClass:[UIScrollView class]])
-        {
+- (UIScrollView *)scrollView {
+    for (UIView *subview in self.pageViewController.view.subviews) {
+        if ([subview isKindOfClass:[UIScrollView class]]){
             return (UIScrollView *)subview;
         }
     }
